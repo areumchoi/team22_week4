92f355c692d63dc230dac686e17f8cb818222bd0
Areumchoi <areumchoi19@gmail.com>
2016-04-02 09:44:42 +0900

    call_bill about user

--- call_bill/src/main/java/team22/call_bill/App.java
+++ call_bill/src/main/java/team22/call_bill/App.java
@@ -5,51 +5,45 @@ import java.util.Scanner;
  *
  */
 class Call_bill{
-	public int time;
-	public int bill;
-	public int line;
-	public String grade;
+	int time;
+	int bill;
+	int line;
+	String grade;
+	private static final String Silver = "silver";
+	private static final String Gold = "gold";
 	
 	public Call_bill(){
+		//객체를 가져오기 위한 생성자
 	}
-	public void base_bill(){
-		if(grade.equals("silver")){
+	public void baseBill(){
+		if(Silver.equals(grade)){
 			bill = 39000;
 		}
-		else if(grade.equals("gold")){
+		else if(Gold.equals(grade)){
 			bill = 59000;
 		}
-		else{
-			System.err.println("GRADE 값이 올바르지 않습니다.");
-		}
 	}
-	public void extra_bill(){
-		if(grade.equals("silver")){
+	public void extraBill(){
+		if(Silver.equals(grade)){
 			if(time>60){
 				bill = bill+((time-60)*540);
 			}
 		}
-		else if(grade.equals("gold")){
+		else if(Gold.equals(grade)){
 			if(time>120){
 				bill = bill+((time-120)*450);
 			}
 		}
-		else{
-			System.err.println("GRADE 값이 올바르지 않습니다.");
-		}
 	}
-	public void line_bill(){
-		if(grade.equals("silver")){
+	public void lineBill(){
+		if(Silver.equals(grade)){
 			bill = bill +((line-1)*39000);
 		}
-		else if(grade.equals("gold")){
+		else if(Gold.equals(grade)){
 			bill = bill + ((line-1)*30000);
 		}
-		else{
-			System.err.println("GRADE 값이 올바르지 않습니다.");
-		}
 	}
-	public void line_discount(){
+	public void lineDiscount(){
 		if(line==3){
 			bill=bill-2000;
 		}
@@ -60,12 +54,12 @@ class Call_bill{
 			bill=bill-4000;
 		}
 	}
-	public int total_bill(){
+	public int totalBill(){
 		int total_bill=0;
-		base_bill();
-		extra_bill();
-		line_bill();
-		line_discount();
+		baseBill();
+		extraBill();
+		lineBill();
+		lineDiscount();
 		total_bill = bill;
 		return total_bill;
 	}
@@ -73,9 +67,9 @@ class Call_bill{
 
 public class App 
 {
-	public static void main( String[] args )
+    public static void main( String[] args )
     {
-		Call_bill call = new Call_bill();
+    	Call_bill call = new Call_bill();
 		Scanner input = new Scanner(System.in);
 		System.out.println("요금제를 입력하시오: ");
 		call.grade = input.next();
@@ -85,6 +79,6 @@ public class App
 		call.time = input.nextInt();
 		
 		System.out.println("당신의 grade는  "+call.grade+"이고, line은 "+call.line+"이며, 사용 시간은 "+call.time+"입니다.");
-		System.out.println("최종 사용 금액은: "+call.total_bill());
+		System.out.println("최종 사용 금액은: "+call.totalBill());
     }
 }
