28d92176a1c353b6cfb11f46fc3044742878b6d3
Hyunji <blackit14@gmail.com>
2016-04-14 23:27:57 +0900

    Divide the method by b1ackit

--- CalculatingBill/src/test/java/team22/CalculatingBill/AppTest.java
+++ CalculatingBill/src/test/java/team22/CalculatingBill/AppTest.java
@@ -36,29 +36,42 @@ public class AppTest
         assertTrue( true );
     }
     
-    public void testSilver(){
-        CallBill callSilver1 = new CallBill("silver", 0, 60);
-        CallBill callSilver2 = new CallBill("silver", 1, 60);
-        CallBill callSilver3 = new CallBill("silver", 2, 60);
-        CallBill callSilver4 = new CallBill("silver", 3, 60);
-        CallBill callSilver5 = new CallBill("silver", 4, 60);
-        CallBill callSilver6 = new CallBill("silver", 5, 60);
-        
+    public void testSilver1(){
+        CallBill callSilver1 = new CallBill("silver", 0, 60);        
         int testSilver1 = callSilver1.totalBill();
-        int testSilver2 = callSilver2.totalBill();
-        int testSilver3 = callSilver3.totalBill();
-        int testSilver4 = callSilver4.totalBill();
-        int testSilver5 = callSilver5.totalBill();
-        int testSilver6 = callSilver6.totalBill();
-
-        
+      
         assertEquals(39000,testSilver1);
-        assertEquals(78000,testSilver2);
-        assertEquals(117000,testSilver3);
-        assertEquals(154000,testSilver4);
-        assertEquals(192000,testSilver5);
-        assertEquals(230000,testSilver6);
-
      }
+    
+    public void testSilver2(){
+        CallBill callSilver2 = new CallBill("silver", 1, 60);
+        int testSilver2 = callSilver2.totalBill();
+        
+        assertEquals(78000,testSilver2);
+    }
+    public void testSilver3(){
+        CallBill callSilver3 = new CallBill("silver", 2, 60);
+        int testSilver3 = callSilver3.totalBill();
+        
+        assertEquals(117000,testSilver3);
+    }
+    public void testSilver4(){
+        CallBill callSilver4 = new CallBill("silver", 3, 60);
+        int testSilver4 = callSilver4.totalBill();
+        
+        assertEquals(154000,testSilver4);
+    }
+    public void testSilver5(){
+        CallBill callSilver5 = new CallBill("silver", 4, 60);
+        int testSilver5 = callSilver5.totalBill();
+        
+        assertEquals(192000,testSilver5);
+    }
+    public void testSilver6(){
+        CallBill callSilver6 = new CallBill("silver", 5, 60);
+        int testSilver6 = callSilver6.totalBill();
+    
+        assertEquals(230000,testSilver6);
+    }
 
 }
